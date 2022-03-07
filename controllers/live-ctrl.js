3ad5adec4c0ff542b321461acef0e2b509d9209d
Tyler242 <77693574+Tyler242@users.noreply.github.com>
2022-03-07 13:33:26 -0700

    added a description

--- controllers/live-ctrl.js
+++ controllers/live-ctrl.js
@@ -4,6 +4,11 @@
 
 // Homepage controller
 exports.getHomePage = (req, res, next) => {
+  // this is an example of how we could do dynamic route creation.
+  // The one thing we would add is we would query the database
+  // to find content related to the pageTitle. If data was found for that
+  // page title, we would send that in our JSON response. If no data
+  // was found we would simply return a 404 error code.
   res.status(200).json({
     pageFound: true,
     pageLevel: 'Home',
